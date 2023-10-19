05eb6f040b11b96b766558ef4612890a07ea3715
beloow <quevaqueva11@gmail.com>
2023-10-19 14:09:27 +0200

    update

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,14 +1,1 @@
-// script bouton go to top
-var btn = $('#button');
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 200) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
 // 
