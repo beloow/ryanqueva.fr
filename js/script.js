f9ab6b2f963f97d093cdfa8c8b6773620d846cef
beloow <quevaqueva11@gmail.com>
2023-12-09 14:51:36 +0100

    - modale

--- js/script.js
+++ js/script.js
@@ -172,22 +172,22 @@ form.addEventListener("submit", (e) => {
 
 /*==================== onload dev ====================*/
 
-window.onload = () => {
+// window.onload = () => {
 
-    Swal.fire({
+//     Swal.fire({
 
-        icon: "info",
-        title: "Tu cherches un alternant motivée ?! Contact moi !",
-        toast: false,
-        backdrop: false,
+//         icon: "info",
+//         title: "Tu cherches un alternant motivée ?! Contact moi !",
+//         toast: false,
+//         backdrop: false,
 
-        position: "top",
-        showConfirmButton: false,
-        timer: 5000,
-        timerProgressBar: true,
+//         position: "top",
+//         showConfirmButton: false,
+//         timer: 5000,
+//         timerProgressBar: true,
 
-        footer: '<a href="#contact" style="color: #FFF; background-color: #1f242d; border-radius:10%; padding: 1rem;">Me contacter !</a>'
+//         footer: '<a href="#contact" style="color: #FFF; background-color: #1f242d; border-radius:10%; padding: 1rem;">Me contacter !</a>'
         
-    });
-}
+//     });
+// }
 
