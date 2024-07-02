3312bf1d8d364eecdb0689698a152d4e02b8b816
beloow <quevaqueva11@gmail.com>
2024-07-02 09:53:59 +0200

    Update du 02/07

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Network Technician', 'Freelancer'],
+    strings: ['Développeur Frontend', 'Technicien réseau', 'Freelance'],
     typeSpeed: 100,
     backSpeed: 90,
     backDelay: 500,
