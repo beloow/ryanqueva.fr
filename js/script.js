5699b41ef2d0d96226fbe3eedd4ba4eca92192ff
beloow <quevaqueva11@gmail.com>
2024-07-12 12:17:07 +0200

    Update du 12/07
    = Traduction complète.
    - Retrait du javascript du contact form.
    + Ajout de phrase d'accroche dans a propos.

--- js/script.js
+++ js/script.js
@@ -62,112 +62,112 @@ const typed = new Typed('.multiple-text', {
 });
 
 /*==================== contact form ====================*/
-const form = document.querySelector('form');
-const fullName = document.getElementById("name");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
-const subject = document.getElementById("subject");
-const mess = document.getElementById("message");
+// const form = document.querySelector('form');
+// const fullName = document.getElementById("name");
+// const email = document.getElementById("email");
+// const phone = document.getElementById("phone");
+// const subject = document.getElementById("subject");
+// const mess = document.getElementById("message");
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    checkInputs();
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     checkInputs();
 
-    if (
-            !fullName.classList.contains("error") &&
-            !email.classList.contains("error") &&
-            !phone.classList.contains("error") &&
-            !subject.classList.contains("error") &&
-            !mess.classList.contains("error")
-        ){
-        sendEmail();
-        form.reset();
-        return false;
-    }
-});
+//     if (
+//             !fullName.classList.contains("error") &&
+//             !email.classList.contains("error") &&
+//             !phone.classList.contains("error") &&
+//             !subject.classList.contains("error") &&
+//             !mess.classList.contains("error")
+//         ){
+//         sendEmail();
+//         form.reset();
+//         return false;
+//     }
+// });
 
-function sendEmail() {
-    const bodyMessage = `
+// function sendEmail() {
+//     const bodyMessage = `
 
-    Name: ${fullName.value}<br> 
-    <br> 
-    Email: ${email.value} <br> 
-    <br> 
-    Téléphone : ${phone.value} <br> 
-    <br> 
-    Message : <br>
-    <br> 
-    ${mess.value}
+//     Name: ${fullName.value}<br> 
+//     <br> 
+//     Email: ${email.value} <br> 
+//     <br> 
+//     Téléphone : ${phone.value} <br> 
+//     <br> 
+//     Message : <br>
+//     <br> 
+//     ${mess.value}
 
-    `;
+//     `;
 
-    Email.send({
+//     Email.send({
 
-        SecureToken: "0f9974e0-e90f-4cd1-a454-aab9a7ad1233",
-        To: 'contact@ryanqueva.fr',
-        From: "contact@ryanqueva.fr",
-        Subject: subject.value,
-        Body: bodyMessage
+//         SecureToken: "0f9974e0-e90f-4cd1-a454-aab9a7ad1233",
+//         To: 'contact@ryanqueva.fr',
+//         From: "contact@ryanqueva.fr",
+//         Subject: subject.value,
+//         Body: bodyMessage
 
-    }).then(
-        message => {
-            if (message == "OK") {
-                Swal.fire({
-                    title: "Good job!",
-                    text: "Email send !",
-                    icon: "success",
-                    showCloseButton: true
-                });
-            }
-        }
-    );
-}
+//     }).then(
+//         message => {
+//             if (message == "OK") {
+//                 Swal.fire({
+//                     title: "Good job!",
+//                     text: "Email send !",
+//                     icon: "success",
+//                     showCloseButton: true
+//                 });
+//             }
+//         }
+//     );
+// }
 
-function checkInputs() {
-    const items = document.querySelectorAll(".item");
+// function checkInputs() {
+//     const items = document.querySelectorAll(".item");
 
-    for (const item of items) {
-        if (item.value == "") {
-            item.classList.add("error");
-            errorBox();
-        }
+//     for (const item of items) {
+//         if (item.value == "") {
+//             item.classList.add("error");
+//             errorBox();
+//         }
 
-        if (items[1].value != "") {
-            checkEmail();
-        }
+//         if (items[1].value != "") {
+//             checkEmail();
+//         }
 
-        items[1].addEventListener("keyup", () => {
-            checkEmail()
-        })
+//         items[1].addEventListener("keyup", () => {
+//             checkEmail()
+//         })
 
-        item.addEventListener("keyup", () => {
-            if (item.value != "") {
-                item.classList.remove("error");
-            } else {
-                item.classList.add("error");
-            }
-        })
-    }
-}
+//         item.addEventListener("keyup", () => {
+//             if (item.value != "") {
+//                 item.classList.remove("error");
+//             } else {
+//                 item.classList.add("error");
+//             }
+//         })
+//     }
+// }
 
-function checkEmail() {
-    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+// function checkEmail() {
+//     const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
 
-    if (!email.value.match(emailRegex)) {
-        email.classList.add("error");
-    } else {
-        email.classList.remove("error");
-    }
-}
+//     if (!email.value.match(emailRegex)) {
+//         email.classList.add("error");
+//     } else {
+//         email.classList.remove("error");
+//     }
+// }
 
-function errorBox() {
-    Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Un champ est incomplet !",
-        showCloseButton: true
-    });
-}
+// function errorBox() {
+//     Swal.fire({
+//         icon: "error",
+//         title: "Oops...",
+//         text: "Un champ est incomplet !",
+//         showCloseButton: true
+//     });
+// }
 
 function PopUpPromo() {
     Swal.fire({
